cbab9c12b9570613abe123d0ecca11a65b9b2b32
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-03-09 20:45:24 +0900

    [Feat] Ray로 상호작용 중인 물체 확인
    ray로 부딪힌 물체를 현재 상호작용 중인 물체로 인식하는 메서드

--- 3D_platformer_pesronal/Assets/04.Scripts/Item/Interaction.cs
+++ 3D_platformer_pesronal/Assets/04.Scripts/Item/Interaction.cs
@@ -8,16 +8,45 @@ public class Interaction : MonoBehaviour
 
     public GameObject curInteractGameObject; // 현재 상호작용 중인 게임오브젝트
     public IInteractable curInteractable; // 인터페이스 참조
-    
+
+
+    // ray에 필요한 변수
+    public float maxCheckDistance; // 최대거리
+    public LayerMask layerMask; // 레이어마스크
+
+    private Camera camera;
     void Start()
     {
-        
+        camera = Camera.main;
     }
 
     void Update()
     {
-        
+        // 물체 감지
+        Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width/2, Screen.height/2));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, maxCheckDistance, layerMask))
+        {
+            // 레이로 검출한 애가 현재 상호작용 중인 게임오브젝트가 아니면, 이걸로 바꿔준다.
+            if (hit.collider.gameObject != curInteractGameObject)
+            {
+                curInteractGameObject = hit.collider.gameObject;
+                // 인터페이스 있는걸로 넣어준다
+                curInteractable = hit.collider.GetComponent<IInteractable>();
+                // 프롬프트 텍스트 띄워주는 함수 호출
+
+            }
+        }
+        // 검출되지 않으면
+        else
+        {
+            curInteractable = null;
+            curInteractGameObject = null;
+
+        }
     }
+
 
     // E키 눌렀을 때
     void OnInteractInput(InputAction.CallbackContext context)
