f71cc0c65cc0f1f1c8644194b143204e07c14659
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-03-09 23:43:07 +0900

    [Feat] 인벤토리ui 초기화
    초기화 관련 메서드

--- 3D_platformer_pesronal/Assets/04.Scripts/UI/UIInventory.cs
+++ 3D_platformer_pesronal/Assets/04.Scripts/UI/UIInventory.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+//using static UnityEditor.Timeline.Actions.MenuPriority;
 
 public class UIInventory : MonoBehaviour
 {
@@ -13,8 +14,10 @@ public class UIInventory : MonoBehaviour
     public Transform slotPanel;
     public Transform dropPosition; // 드롭 위치
 
-    // 선택한 아이템
+    // 슬롯 중에서 선택한 아이템
     [Header("Selected Item")]
+    private ItemSlot selectedItem;
+    private int selectedItemIndex;
     public TextMeshProUGUI selectedItemName;
     public TextMeshProUGUI selectedItemNDescription;
     public TextMeshProUGUI selectedItemStatName;
@@ -37,9 +40,34 @@ public class UIInventory : MonoBehaviour
         controller = CharacterManager.Instance.Player.controller;
         conditions = CharacterManager.Instance.Player.condition;
         dropPosition = CharacterManager.Instance.Player.dropPositon;
+
+        controller.inventory += Toggle;
+        //CharacterManager.Instance.Player.AddItem += AddItem;
+
+        inventoryWindow.SetActive(false);
+
+        // slots 밑에 딸린 itemslot.cs 달린 슬롯 개수 만큼 객체 생성
+        slots = new ItemSlot[slotPanel.childCount];
+
+        for (int i=0; i<slots.Length; i++)
+        {
+            slots[i] = slotPanel.GetChild(i).GetComponent<ItemSlot>(); 
+            slots[i].index = i;
+            slots[i].uiInventory = this; 
+            //slots.set();
+        }
+
+        ClearSelectedItemWindow();
     }
 
-
+    void ClearSelectedItemWindow()
+    {
+        selectedItem = null;
+        selectedItemName.text = string.Empty;
+        selectedItemNDescription.text = string.Empty;
+        selectedItemStatName.text = string.Empty;
+        selectedItemStatValue.text = string.Empty;
+    }
     // 인벤토리 열고 닫기
     public void Toggle()
     {
