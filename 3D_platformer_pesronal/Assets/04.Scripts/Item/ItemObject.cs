566ed192c9f6f577f99fdcabd134c7f2c43193b3
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-03-09 20:25:15 +0900

    [Feat] OnInteract() 메서드 생성
    상호작용(E)를 눌렀을 때 호출될 메서드

--- 3D_platformer_pesronal/Assets/04.Scripts/Item/ItemObject.cs
+++ 3D_platformer_pesronal/Assets/04.Scripts/Item/ItemObject.cs
@@ -20,6 +20,11 @@ public class ItemObject : MonoBehaviour,IInteractable
 
     public void OnInteract()
     {
-        
+        // 아이템데이터에 넣어줌
+        CharacterManager.Instance.Player.ItemData = itemData;
+        // 액션
+        CharacterManager.Instance.Player.AddItem.Invoke();
+        // E누르면 아이템 먹은거니까 없애줌
+        Destroy(gameObject);
     }
 }
