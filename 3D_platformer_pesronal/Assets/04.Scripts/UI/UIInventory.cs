46ef3d0dc7844c078bd3d33a7842caf21f07c50d
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-03-10 01:54:16 +0900

    [Feat] RemoveSelctedItem()
    버튼선택후 호출할 함수,
    선택한 아이템을 초기화

--- 3D_platformer_pesronal/Assets/04.Scripts/UI/UIInventory.cs
+++ 3D_platformer_pesronal/Assets/04.Scripts/UI/UIInventory.cs
@@ -49,9 +49,9 @@ public class UIInventory : MonoBehaviour
         // slots 밑에 딸린 itemslot.cs 달린 슬롯 개수 만큼 객체 생성
         slots = new ItemSlot[slotPanel.childCount];
 
-        for (int i=0; i<slots.Length; i++)
+        for (int i = 0; i < slots.Length; i++)
         {
-            slots[i] = slotPanel.GetChild(i).GetComponent<ItemSlot>(); 
+            slots[i] = slotPanel.GetChild(i).GetComponent<ItemSlot>();
             slots[i].index = i;
             slots[i].uiInventory = this;
             slots[i].Clear();
@@ -73,7 +73,7 @@ public class UIInventory : MonoBehaviour
     {
         for (int i = 0; i < slots.Length; i++)
         {
-            if(slots[i].ItemData != null)
+            if (slots[i].ItemData != null)
             {
                 slots[i].Set();
             }
@@ -87,7 +87,7 @@ public class UIInventory : MonoBehaviour
     // 인벤토리 열고 닫기
     public void Toggle()
     {
-        if (IsOpen()) 
+        if (IsOpen())
         {
             inventoryWindow.SetActive(false);
         }
@@ -101,11 +101,11 @@ public class UIInventory : MonoBehaviour
     {
         return inventoryWindow.activeInHierarchy;
     }
-    
+
     // 중복 가능한 슬롯 확인하고 반환하는 메서드
     ItemSlot GetItemStack(ItemData data)
     {
-        for (int i = 0; i < slots.Length; i++) 
+        for (int i = 0; i < slots.Length; i++)
         {
             // 슬롯의 아이템과 넣으려는 아이템이 같고 / 아이템 개수가 최대를 넘지 않을때 
             if (slots[i].ItemData == data && slots[i].quantity < data.maxStackAmount)
@@ -113,12 +113,12 @@ public class UIInventory : MonoBehaviour
                 return slots[i];
             }
         }
-        return null ;
+        return null;
     }
 
     // 빈 슬롯 찾아서 반환 메서드
     ItemSlot GetEmptySlot()
-    { 
+    {
         for (int i = 0; i < slots.Length; i++)
         {
             // 아이템 안들어있을 때 그 슬롯 반환
@@ -148,7 +148,7 @@ public class UIInventory : MonoBehaviour
                 return;
             }
         }
-        
+
         // 중복 가능하지 않은 아이템들은 빈 슬롯을 찾는다
 
         ItemSlot emptySlot = GetEmptySlot();
@@ -167,7 +167,7 @@ public class UIInventory : MonoBehaviour
         ThrowItem(itemData);
         CharacterManager.Instance.Player.ItemData = null;
     }
-    
+
     public void ThrowItem(ItemData itemData)
     {
         Instantiate(itemData.dropPrefab, dropPosition.position, Quaternion.Euler(Vector3.one * Random.value * 360));
@@ -175,8 +175,8 @@ public class UIInventory : MonoBehaviour
 
 
     /// ui 관련
-    
-    
+
+    // 선택한 아이템을 정보창에 업데이트 해주는 함수
     public void SelectItem(int index)
     {
         if (slots[index].ItemData == null) return;
@@ -201,8 +201,63 @@ public class UIInventory : MonoBehaviour
         unEquipButton.SetActive(selectedItem.ItemData.type == ItemType.Equipable && slots[index].equipped);
 
         dropButton.SetActive(true);
-        
-
-        
     }
+
+    // 버튼 선택 후에 선택했던 아이템 지워주는 함수 
+    void RemoveSelctedItem()
+    {
+        selectedItem.quantity--;
+
+        // 1개도 없으면
+        if (selectedItem.quantity <= 0)
+        {
+            //착용을 하고 있다면 벗는다
+            if (slots[selectedItemIndex].equipped)
+            {
+                //
+            }
+
+            // 착용템 아니면
+            selectedItem.ItemData = null;
+            ClearSelectedItemWindow();
+        }
+        UpdateUI();
+    }
+
+    // #1사용하기 버튼
+
+    public void OnUeseButton()
+    {
+        // 소비 타입일 경우
+        if (selectedItem.ItemData.type == ItemType.Consumable)
+        {
+            for (int i = 0; i < selectedItem.ItemData.ItemsConsumables.Length; i++)
+            {
+                switch (selectedItem.ItemData.ItemsConsumables[i].type)
+                {
+                    // 소비 타입에 따른 함수 호출
+                    case ConsumableType.Health:
+                        conditions.Eat(selectedItem.ItemData.ItemsConsumables[i].value); break;
+                }
+            }
+            RemoveSelctedItem(); // 선택한 아이템 초기화
+        }
+    }
+
+
+
+    
+    // # 2 착용하기 버튼
+
+    // # 3 해제하기 버튼
+
+    // #4  버리기 버튼
+
+    public void OnDropButton()
+    {
+
+        inventoryWindow.SetActive(false);
+    }
+
+
 }
