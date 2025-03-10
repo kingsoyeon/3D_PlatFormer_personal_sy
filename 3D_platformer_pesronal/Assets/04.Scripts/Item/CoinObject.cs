c94f4921f83391ed7480396b36e1a461a1ba601c
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-03-10 16:14:57 +0900

    [Feat] coin-플레이어 충돌 구현
    coin과 충돌시 코인 비활성화
    -> ui에 업데이트 구현 필요
    ->> 추후 재생성되는 로직 추가

---
+++ 3D_platformer_pesronal/Assets/04.Scripts/Item/CoinObject.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinObject : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("ÄÚÀÎ È¹µæ");
+        this.gameObject.SetActive(false);
+
+        // UI¿¡ ¿¬°á
+    }
+}
