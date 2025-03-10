1c9af932adc04877b6a7550837e049a1a539ad2b
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-03-10 09:30:28 +0900

    [Feat] Equiptool.cs 변수선언
    공격주기,공격여부,공격사거리에 대한 변수 및 무기의 타입을 bool로 확인

--- 3D_platformer_pesronal/Assets/04.Scripts/EquipTool.cs
+++ 3D_platformer_pesronal/Assets/04.Scripts/EquipTool.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class EquipTool : Equip
 {
-    
-    void Start()
-    {
-        
-    }
+    // 장비 정보 //
 
-    void Update()
-    {
-        
-    }
+    public float attackRate; // 공격주기
+    private bool attacking; // 공격여부
+    public float attackDistance; // 공격 사거리
+
+    [Header("Resource Gathering")]
+    public bool doesGatherResources; // 자원을 모으는 도구인지
+
+    [Header("Combat")]
+    public bool doesDealDamage; // 데미지를 주는 무기인지
+    public int damage;
 }
