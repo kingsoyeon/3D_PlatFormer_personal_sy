cab125e7410fe12de6a723fa44650bd8d2eca2cd
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-03-11 01:22:31 +0900

    [Feat] LaserTrapZone 구현
    플레이어가 동굴을 지날 때 감지하고 텍스트를 띄움

--- 3D_platformer_pesronal/Assets/04.Scripts/UI/LaserTrap.cs
+++ 3D_platformer_pesronal/Assets/04.Scripts/UI/LaserTrap.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class LaserTrap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // 띄울 메세지
+    public TextMeshProUGUI TrapText;
 
-    // Update is called once per frame
+    // 플레이어 감시 구간
+    [SerializeField] private Vector3 boxCenter;
+    [SerializeField] private Vector3 boxSize = new Vector3(2f, 2f, 2f);
+
+    public LayerMask playerLayer;
+
+    private bool hasPopuped = false; // 이전에 팝업이 되었었는지
+
+    private void Start()
+    {
+        TrapText.gameObject.SetActive(false);
+    }
     void Update()
     {
-        
+        DetectPlayer();
+    }
+
+    void DetectPlayer()
+    {
+        // 박스의 좌표를 월드좌표로 변환
+        Vector3 boxWorldCenter = transform.position + boxCenter;
+
+        // 플레이어 레이어마스크 설정 필수
+        Collider[] hits = Physics.OverlapBox(boxWorldCenter, boxSize / 2, Quaternion.identity, playerLayer);
+
+        // 플레이어가 감지가 되면
+        if (hits.Length > 0 && !hasPopuped)
+        {
+            
+            hasPopuped = true;
+            StartCoroutine(PopupText());
+        }  
+    }
+
+    private IEnumerator PopupText()
+    {
+        TrapText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        TrapText.gameObject.SetActive(false);
     }
 }
