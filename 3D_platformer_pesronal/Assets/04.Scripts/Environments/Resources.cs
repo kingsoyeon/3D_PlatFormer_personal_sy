b071216c2442d40a05a5cd441afc2583a3a4adac
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-03-11 15:27:46 +0900

    [Feat] 블록 아이템 랜덤보상
    블록을 깨면 랜덤한 아이템이 3개가 드랍된다

--- 3D_platformer_pesronal/Assets/04.Scripts/Environments/Resources.cs
+++ 3D_platformer_pesronal/Assets/04.Scripts/Environments/Resources.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Resources : MonoBehaviour
 {
-    public ItemData itemToGive; // 드랍할 아이템
+    public ItemData[] itemToGive; // 드랍할 아이템
     public int quantityPerHit = 1; // 한 번당 드랍할 아이템 갯수
     public int capacity; // 칠 수 있는 횟수
 
@@ -18,8 +18,14 @@ public class Resources : MonoBehaviour
             
         }
         if (capacity <= 0) 
-        { Destroy(gameObject); 
-         Instantiate(itemToGive.dropPrefab, hitPoint + Vector3.up, Quaternion.LookRotation(hitNormal, Vector3.up)); 
+        {    Destroy(gameObject);
+
+            for (int i = 0; i < 3; i++)  // 깨질 때 3개의 아이템 드랍
+            {
+                ItemData randomItem = itemToGive[Random.Range(0, itemToGive.Length)];
+
+                Instantiate(randomItem.dropPrefab, hitPoint + Vector3.up, Quaternion.LookRotation(hitNormal, Vector3.up));
+            }
         }
     }
 }
